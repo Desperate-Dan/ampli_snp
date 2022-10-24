995fa1d9736370a6c62e33dee4b8676c3f52cb7b
Daniel M Maloney <84576669+Desperate-Dan@users.noreply.github.com>
2022-10-24 17:09:03 +0100

    Create setup.py
    Testing initial setup.py file.

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+from setuptools.command.build_py import build_py
+import glob
+import os
+import pkg_resources
+
+setup(
+    name='ampli_snp',
+    version='0.0.1',    
+    description='ampli_snp',
+    url='https://github.com/Desperate-Dan/ampli_snp',
+    author='Daniel Maloney',
+    author_email='dmaloney@ed.ac.uk',
+    license='...',
+    packages=find_packages(),
+    scripts=['ampli_snp/ampli_snp/function_file.py']
+    install_requires=['biopython'
+                      ],
+    entry_points="""
+    [console_scripts]
+    {program} = ampli_snp.command:main
+    """.format(program = _program),
+    include_package_data=True
+)
